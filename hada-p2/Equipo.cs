3562df370f124c7c3bc1fa7f93531b098b10960c
Rayane Chelihi Chelouche <rcc85@gcloud.ua.es>
2023-03-09 18:19:17 +0100

    51257925X Añadida la clase Program y la clase Equipo, he quitado el antiguo program.

---
+++ hada-p2/Equipo.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hada
+{
+	class Equipo
+	{
+		public static int minJugadores { get; set; }
+		public static int maxNumeroMovimientos { get; set; }
+
+		public int movimientos { get; private set; }
+		public string nombreEquipo { get; private set; }
+		List<Jugador> jugadores { get; set; }
+		List<Jugador> expulsados { get; set; }
+		List<Jugador> lesionados { get; set; }
+		List<Jugador> retirados { get; set; }
+
+
+		public Equipo(int bj, string nom)
+		{
+			jugadores = new List<Jugador>();
+			expulsados = new List<Jugador>();
+			lesionados = new List<Jugador>();
+			retirados = new List<Jugador>();
+
+			for (int i = 0; i < bj; i++)
+			{
+				Jugador j = new Jugador("Juagdor_" + (i + 1).ToString(), 0, 0, 50, 0);
+				j.amonestacionesMaximoExcedido += cuandoAmonestacionesMaximoExcedido;
+				j.faltasMaximoExcedido += cuandoFaltasMaximoExcedido;
+				j.energiaMinimaExcedida += cuandoEnergiaMinimaExcedida;
+				jugadores.Add(j);
+			}
+			nombreEquipo = nom;
+		}
+		public bool moverJugadores()
+		{
+			int movidos = 0;
+			movimientos++;
+			if (jugadores.Count > minJugadores)
+			{
+				foreach (var j in jugadores)
+				{
+					if (j.todoOk())
+					{
+						j.mover();
+						movidos++;
+					}
+				}
+			}
+			if (movidos > minJugadores)
+				return true;
+			return false;
+		}
+		public void moverJugadoresEnBucle()
+		{
+			bool ok = true;
+			while (ok == true)
+			{
+				ok = false;
+				foreach (var j in jugadores)
+				{
+					if (j.todoOk())
+					{
+						j.mover();
+						ok = true;
+					}
+				}
+			}
+		}
+		public int sumarPuntos()
+		{
+			int puntos = 0;
+			foreach (var j in jugadores)
+			{
+				puntos += j.puntos;
+			}
+			return puntos;
+		}
+		public List<Jugador> getJugadoresExcedenLimiteAmonestaciones()
+		{
+			return expulsados;
+		}
+		public List<Jugador> getJugadoresExcedenLimiteFaltas()
+		{
+			return lesionados;
+		}
+
+		public List<Jugador> getJugadoresExcedenMinimoEnergia()
+		{
+			return retirados;
+		}
+
+		public override string ToString()
+		{
+			string ss = "[" + nombreEquipo + "]Puntos: " + sumarPuntos().ToString() + "; Expulsados: " + expulsados.Count + "; Lesionados: " + lesionados.Count + "; Retirados: " + retirados.Count;
+			ss += "\n";
+			foreach (var j in jugadores)
+			{
+				ss += j.ToString() + "\n";
+			}
+			return ss;
+		}
+
+		private void cuandoAmonestacionesMaximoExcedido(object sender, AmonestacionesMaximoExcedidoArgs args)
+		{
+			Jugador j = (Hada.Jugador)sender;
+			Console.WriteLine("¡¡Número máximo excedido de amonestaciones. Jugador expulsado!!");
+			Console.WriteLine("Jugador: " + j.nombre);
+			Console.WriteLine("Equipo: " + nombreEquipo);
+			Console.WriteLine("Amonestaciones: " + args.amonestaciones);
+			expulsados.Add(j);
+		}
+		private void cuandoFaltasMaximoExcedido(object sender, FaltasMaximoExcedidoArgs args)
+		{
+			Jugador j = (Hada.Jugador)sender;
+			Console.WriteLine("¡¡Número máximo excedido de faltas recibidas. Jugador lesionado!!");
+			Console.WriteLine("Jugador: " + j.nombre);
+			Console.WriteLine("Equipo: " + nombreEquipo);
+			Console.WriteLine("Faltas: " + args.faltas);
+			lesionados.Add(j);
+		}
+		private void cuandoEnergiaMinimaExcedida(object sender, EnergiaMinimaExcedidaArgs args)
+		{
+			Jugador j = (Hada.Jugador)sender;
+			Console.WriteLine("¡¡Energía mínima excedida. Jugador retirado!!");
+			Console.WriteLine("Jugador: " + j.nombre);
+			Console.WriteLine("Equipo: " + nombreEquipo);
+			Console.WriteLine("Energia: " + args.energia + " %");
+			retirados.Add(j);
+		}
+	}
+}
---
+++ hada-p2/Program .cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa principal");
+
+            // Asignamos las variables estaticas de Jugador
+            Jugador.maxAmonestaciones = 6; // Numero maximo de amonestaciones permitidas para ser expulsado
+            Jugador.maxFaltas = 8; // Numero maximo de faltas recibidas para lesionarse
+            Jugador.minEnergia = 1; // Energia del jugador
+            Jugador.rand = new Random();
+
+            // Asignamos las variables estaticas de Equipo
+            Equipo.maxNumeroMovimientos = 8; // numero máximo de movimientos del partido
+            Equipo.minJugadores = 2; // numero minimo de jugadores permitido por equipo
+
+            // Prueba 1 (Ejemplo de como comprobar que el jugador se mueve correctamente)
+            //ProbarMoverJugador();
+
+            // JUGAR PARTIDO
+            
+            // Creamos 2 equipos de 6 jugadores
+            Equipo equipoA = new Equipo(6, "equipo_A");
+            Equipo equipoB = new Equipo(6, "equipo_B");
+
+            // Generamos la partida
+            int estado = GenerarPartida(equipoA, equipoB);
+
+            // Mostrar el resultado del partido en funcion del estado
+            Console.WriteLine("------------------------------------------------");
+            MostrarResultado(equipoA, equipoB, estado);
+
+            Console.WriteLine("Pulse una tecla para finalizar");
+            Console.ReadLine();
+        }
+
+        /* Juega el partido. Devuelve el estado en que quedará el partido
+        1 -> Equipo B pierde porque no puede mover
+        2 -> Equipo A pierde porque no puede mover   
+        3 -> Llegamos al final del partido, por lo que compararemos puntos después
+        4 -> Los dos equipos pierden, por lo tanto empatan
+        */
+
+        public static int GenerarPartida(Equipo equipoA, Equipo equipoB)
+        {
+            bool estadoA, estadoB;
+            int estado = -1;
+            
+            do
+            {
+                estadoA = equipoA.moverJugadores();
+                estadoB = equipoB.moverJugadores();
+
+                Console.WriteLine("Movimiento " + equipoA.movimientos);
+
+                Console.WriteLine(equipoA);
+                Console.WriteLine(equipoB);
+
+                if (!estadoA)
+                {
+                    if (!estadoB)
+                        estado = 4; // Los dos equipos pierden por lo tanto empatan
+                    else
+                    {
+                        // Equipo A pierde porque no puede mover
+                        estado = 2;
+                    }
+                    break;
+                }
+                else
+                {
+                    if (!estadoB)
+                    {
+                        // Equipo B pierde porque no puede mover
+                        estado = 1;
+                        break;
+                    }
+                }
+
+
+                // En caso de llegar al numero maximo de movimiento, comparar puntos
+                if (equipoA.movimientos>Equipo.maxNumeroMovimientos)
+                {
+                    estado = 3;
+                    break;
+                }
+
+            } while (estadoA && estadoB);
+
+
+            return estado;     
+        }
+
+        // Muestra el resultado del partido según el estado
+        public static void MostrarResultado(Equipo eA, Equipo eB, int estado)
+        {
+            switch (estado)
+            {
+                case 1: // Gana el equipo A (porque el equipo B ha sido descalificado)
+                    Console.WriteLine("Gana el [" + eA.nombreEquipo + "] utilizando " + eA.movimientos + " movimientos.");
+                    break;
+                case 2: // Gana el equipo B (porque el equipo A ha sido descalificado)
+                    Console.WriteLine("Gana el [" + eB.nombreEquipo + "] utilizando " + eB.movimientos + " movimientos.");
+                    break;
+                case 3:
+                    // Hemos llegado al final de la partida (maxNumeroMoviimentos)
+                    int puntosA = eA.sumarPuntos();
+                    int puntosB = eB.sumarPuntos();
+
+                    // Empate
+                    if (puntosA == puntosB)
+                    {
+                        Console.WriteLine("El [" + eA.nombreEquipo + "] y el [" + eB.nombreEquipo + "] han empatado utilizando " + eA.movimientos + " movimientos.");
+                    }
+                    else
+                    {
+                        // Gana A
+                        if (puntosA > puntosB)
+                        {
+                            Console.WriteLine("Gana el equipo [" + eA.nombreEquipo + "] utilizando " + eA.movimientos + " movimientos.");
+                        }
+                        else // Gana B
+                            Console.WriteLine("Gana el equipo [" + eB.nombreEquipo + "] utilizando " + eB.movimientos + " movimientos.");
+
+                    }
+
+                    break;
+                case 4: // Los dos equipos quedan descalificados por lo tanto quedan empatados
+                    Console.WriteLine("El [" + eA.nombreEquipo + "] y el [" + eB.nombreEquipo + "] han empatado utilizando " + eA.movimientos + " movimientos (descalificados).");
+                    break;
+
+            }
+
+            // Mostrar el estado de los equipos
+            Console.WriteLine(eA);
+            Console.WriteLine();
+            Console.WriteLine(eB);
+
+        }
+
+        // Probar que el jugador se mueve correctamente
+        public static void ProbarMoverJugador()
+        {
+            string nombre = "Leo Messi";
+            int amonestaciones = 0;
+            int faltas = 0;
+            int energia = 50;
+            int puntos = 0;
+
+            Jugador j = new Jugador(nombre, amonestaciones, faltas, energia, puntos);
+
+            // Estado inicial
+            Console.WriteLine("Estado inicial del jugador");
+            Console.WriteLine(j);
+            Console.WriteLine();
+
+            // Movimiento manual
+            Console.WriteLine("Estado después de mover manualmente");
+            j.incAmonestaciones();
+            j.incFaltas();
+            j.decEnergia();
+            j.incPuntos();
+
+            Console.WriteLine(j);
+            Console.WriteLine();
+
+            // Movimiento automatico del jugador
+            Console.WriteLine("Estado después de mover automáticamente (funcion mover())");
+            j.mover();
+            Console.WriteLine(j);
+            Console.WriteLine();
+        }
+    }
+}
